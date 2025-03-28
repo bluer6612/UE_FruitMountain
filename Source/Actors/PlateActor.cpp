f465cf39ddac35a41b35dd4e31a8bc88eb3861b6
bluer6612 <gomi6612@gmail.com>
2025-03-28 10:39:29 +0900

    문병무: plate 에셋 추가

--- Source/Actors/PlateActor.cpp
+++ Source/Actors/PlateActor.cpp
@@ -7,16 +7,16 @@ APlateActor::APlateActor()
     PrimaryActorTick.bCanEverTick = false;
     RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
 
-    static ConstructorHelpers::FObjectFinder<UStaticMesh> CylinderAsset(TEXT("/Game/Asset/Mesh_Bowl"));
-    if (!CylinderAsset.Succeeded())
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> PlateAsset(TEXT("/Game/Asset/Plate1"));
+    if (!PlateAsset.Succeeded())
     {
-        UE_LOG(LogTemp, Warning, TEXT("CylinderAsset not found!"));
+        UE_LOG(LogTemp, Warning, TEXT("PlateAsset not found!"));
         return;
     }
 
     UStaticMeshComponent* PlateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlateMesh"));
     PlateMesh->SetupAttachment(RootComponent);
-    PlateMesh->SetStaticMesh(CylinderAsset.Object);
+    PlateMesh->SetStaticMesh(PlateAsset.Object);
 
     // 원판을 얇게, 25.f 위에 배치
     PlateMesh->SetRelativeScale3D(FVector(20.f, 20.f, 0.1f));
