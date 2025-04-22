b286ae0625718194710a6dacd07667c5ff6b96b3
bluer6612 <gomi6612@gmail.com>
2025-04-22 13:08:06 +0900

    문병무: 토탈 점수 위젯 세부 조정 완료

--- Source/Interface/UI/TotalScoreWidget.cpp
+++ Source/Interface/UI/TotalScoreWidget.cpp
@@ -11,7 +11,7 @@ UTotalScoreWidget* UTotalScoreWidget::Instance = nullptr;
 TSubclassOf<UUserWidget> UTotalScoreWidget::TotalScoreWidgetClass = nullptr;
 
 // UI_Play_Score 위젯 위에 겹치게 위치 설정
-const FVector2D UTotalScoreWidget::TOTALSCORE_TEXT_POS = FVector2D(292.0f, 130.0f);
+const FVector2D UTotalScoreWidget::TOTALSCORE_TEXT_POS = FVector2D(292.0f, 106.5f);
 
 UTotalScoreWidget::UTotalScoreWidget(const FObjectInitializer& ObjectInitializer)
     : Super(ObjectInitializer)
